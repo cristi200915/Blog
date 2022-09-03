19034888ad77ac83daac8d1a232c232bfe886b2b
Cristian Ortiz <cristi200915@hotmail.com>
2022-09-02 21:34:10 -0500

    Fonts fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,8 @@
     <title>Blog Laravel App</title>
     <base href="<?=url('/')?>/">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="assets/js/<?=$style?>">
 </head>
